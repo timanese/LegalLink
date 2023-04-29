67d35143c44d3579be66f07fea9442120c517e0a
Nicole Shaq Sammy <shaq@Nicole--Sammy.local>
2023-04-29 10:11:05 -0400

    added gpt.js and started training data file

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 // Import dependencies
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require( 'dotenv' );
-const { getImageDescription } = require('./utils/imageToText.js');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const { getImageDescription } = require("./utils/imageToText.js");
+const { generateText } = require("./utils/gpt");
 
 // Load environment variables from .env file
 dotenv.config();
@@ -16,14 +17,17 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
+    useUnifiedTopology: true,
+  })
+  .then(() => {
     console.log("MongoDB connected!");
-}).catch((err) => {
+  })
+  .catch((err) => {
     console.error("MongoDB connection error:", err);
-});
+  });
 
 // Connect to OpenAI
 const { Configuration, OpenAIApi } = require("openai");
@@ -33,18 +37,26 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 // Define API routes
-const caseRoutes = require('./routes/caseRoutes');
-const clientRoutes = require('./routes/clientRoutes');
+const caseRoutes = require("./routes/caseRoutes");
+const clientRoutes = require("./routes/clientRoutes");
 // const intakeFormRoutes = require('./routes/intakeFormRoutes');
-const mailRoutes = require('./routes/mailRoutes');
+const mailRoutes = require("./routes/mailRoutes");
 
-app.use('/api/cases', caseRoutes);
-app.use('/api/clients', clientRoutes);
+app.use("/api/cases", caseRoutes);
+app.use("/api/clients", clientRoutes);
 // app.use('/api/intake-forms', intakeFormRoutes);
-app.use('/api/mail', mailRoutes);
+app.use("/api/mail", mailRoutes);
 
 // Start the server
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-    console.log( `Server started on port ${PORT}` );
+app.listen(PORT, async () => {
+  console.log(`Server started on port ${PORT}`);
+  // Replace 'caseDescription' with the actual description you want to use
+  const caseDescription =
+    "A man is suing his neighbor for playing loud music at night.";
+
+  const prompt = `Grade the following legal case based on its description: '${caseDescription}'. Provide a grade from 1 to 10, where 1 is the weakest and 10 is the strongest. For reference, a grade 1 case might be: 'A person is suing their neighbor for the color of their mailbox', while a grade 10 case might be: 'A person is suing a large corporation for causing irreversible environmental damage resulting in the loss of their livelihood and severe health issues'.`;
+
+  const response = await generateText(prompt);
+  console.log("AI response:", response);
 });
