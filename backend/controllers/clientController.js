7f7ee281d7d9cf4df32ec463635ddd6499ed11e5
ZacharyEbaugh <zlebaughwps@gmail.com>
2023-04-29 01:25:38 -0400

    login/register endpoints complete

--- backend/controllers/clientController.js
+++ backend/controllers/clientController.js
@@ -3,14 +3,21 @@ const Client = require('../models/Client');
 // Register a new client
 exports.register = async (req, res) => {
   try {
-    const { name, email, phone } = req.body;
+    const { name, email, password, phone } = req.body;
 
+    // Check if all information is provided
+    if (!name || !email || !password || !phone) {
+      return res.status(400).json({ message: 'Please provide all information' });
+    }
+
+    // Check if the user already exists
     const existingClient = await Client.findOne({ email });
     if (existingClient) {
       return res.status(400).json({ message: 'User already exists' });
     }
 
-    const client = new Client({ name, email, phone });
+    // Create a new client
+    const client = new Client({ name, email, password, phone });
     await client.save();
 
     res.status(201).json({ message: 'User registered successfully', client });
@@ -23,11 +30,16 @@ exports.register = async (req, res) => {
 // Login a client
 exports.login = async (req, res) => {
   try {
-    const { email } = req.body;
+    const { email, password } = req.body;
 
     const client = await Client.findOne({ email });
     if (!client) {
       return res.status(400).json({ message: 'User not found' });
+    }
+
+    // Check if the submitted password matches the one in the database
+    if (client.password !== password) {
+      return res.status(400).json({ message: 'Invalid password' });
     }
 
     res.status(200).json({ message: 'Logged in successfully', client });
