a66fd2833e08b136e5e0909d1bec2ed2961b464c
willortiz7 <willortiz407@gmail.com>
2023-04-29 15:37:40 -0400

    Update app bar color

--- legal-link-ui/src/pages/ClientCaseView.js
+++ legal-link-ui/src/pages/ClientCaseView.js
@@ -26,6 +26,7 @@ const drawerWidth = 240;
 const AppBar = styled(MuiAppBar, {
   shouldForwardProp: (prop) => prop !== "open",
 })(({ theme, open }) => ({
+  backgroundColor: "#2257bf",
   zIndex: theme.zIndex.drawer + 1,
   transition: theme.transitions.create(["width", "margin"], {
     easing: theme.transitions.easing.sharp,
