ef0a3eef11442c51522640e66a90ce572d384e42
ZacharyEbaugh <zlebaughwps@gmail.com>
2023-04-29 00:31:20 -0400

    Connect to OpenAI and merge conflict

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,46 @@
-// Import environment variables
-require('dotenv').config();
-
-// Establish express app and middleware
+// Import dependencies
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Initialize Express app
 const app = express();
-const port = process.env.PORT || 3001;
-const openai = require('openai');
 
-// Host port 3001
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
-    });
+// Apply middleware
+app.use(cors());
+app.use(express.json());
 
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("MongoDB connected!");
+}).catch((err) => {
+    console.error("MongoDB connection error:", err);
+});
+
+// Connect to OpenAI
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+// Define API routes
+// const caseRoutes = require('./routes/caseRoutes');
+// const clientRoutes = require('./routes/clientRoutes');
+// const intakeFormRoutes = require('./routes/intakeFormRoutes');
+// app.use('/api/cases', caseRoutes);
+// app.use('/api/clients', clientRoutes);
+// app.use('/api/intake-forms', intakeFormRoutes);
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
