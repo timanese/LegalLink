a72cc2d4b3c6c125cb98ec50d7b3fae3f2cd3fda
willortiz7 <willortiz407@gmail.com>
2023-04-29 17:14:56 -0400

    Add content to message modal

--- legal-link-ui/src/components/MessageModal.js
+++ legal-link-ui/src/components/MessageModal.js
@@ -1,3 +1,4 @@
+import { Button, TextField } from "@mui/material";
 import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
 import Typography from "@mui/material/Typography";
@@ -8,9 +9,9 @@ const style = {
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 400,
+  width: 500,
   bgcolor: "background.paper",
-  border: "2px solid #000",
+  border: "2px solid #2257bf",
   boxShadow: 24,
   p: 4,
 };
@@ -27,12 +28,32 @@ export default function MessageModal(props) {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
+          <Typography
+            id="modal-modal-title"
+            variant="h6"
+            component="h2"
+            sx={{ pb: 4 }}
+          >
+            Message Client
           </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
+          <TextField
+            minRows={5}
+            maxRows={10}
+            multiline
+            sx={{ width: "100%" }}
+          ></TextField>
+          <Box
+            sx={{
+              display: "flex",
+
+              justifyContent: "flex-end",
+              pt: 4,
+            }}
+          >
+            <Button variant="contained" onClick={() => setIsOpen(false)}>
+              Send
+            </Button>
+          </Box>
         </Box>
       </Modal>
     </div>
