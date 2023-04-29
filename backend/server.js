0e2f480fba43d7a6b60b96a8d546954b73f9017c
Nicole Shaq Sammy <shaq@Nicole--Sammy.local>
2023-04-29 13:17:19 -0400

    added GPT grade to createcase

--- backend/server.js
+++ backend/server.js
@@ -57,15 +57,6 @@ app.listen(PORT, async () => {
   const caseDescription =
     "'I rented an apartment that was infested with mold, which the landlord failed to disclose. As a result, I developed respiratory issues and had to seek medical treatment. The landlord has refused to address the mold problem or compensate me for my medical expenses.";
 
-  const prompt = `Based on the description provided, please grade the following legal case on a scale from 1 to 10, where 1 signifies the least likelihood of winning a court case or receiving a small settlement, and 10 represents the highest likelihood of winning a court case and obtaining a very large settlement.
-
-Case Description: '${caseDescription}'
-
-For reference:
-- A Grade 1 case example: 'A person is suing their neighbor over the color of their mailbox.'
-- A Grade 10 case example: 'A person is suing a large corporation for causing irreversible environmental damage, resulting in the loss of their livelihood and severe health issues.'
-Please only give the grade without an explanation`;
-
-  const response = await generateText(prompt);
+  const response = await generateText(caseDescription);
   console.log("AI response:", response);
 });
