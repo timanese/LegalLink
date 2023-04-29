b34fadef22b5cbb4d7909a8c671c97a23a4238f8
Nicole Shaq Sammy <shaq@Nicole--Sammy.local>
2023-04-29 13:43:50 -0400

    fixed issue with test_doc.js

--- backend/test_doc.js
+++ backend/test_doc.js
@@ -1,17 +1,20 @@
 /**
  * TODO(developer): Uncomment these variables before running the sample.
  */
-const projectId = 'lustrous-bonito-353500';
-const location = 'us'; // Format is 'us' or 'eu'
-const processorId = '164bb6b240e87770'; // Create processor in Cloud Console
-const filePath = '/Users/zacharyebaugh/Documents/LegalLink/backend/American Government_ Elections.pdf';
+const projectId = "lustrous-bonito-353500";
+const location = "us"; // Format is 'us' or 'eu'
+const processorId = "164bb6b240e87770"; // Create processor in Cloud Console
+const filePath = "./American Government_ Elections.pdf";
 
-const {DocumentProcessorServiceClient} =
-  require('@google-cloud/documentai').v1;
+const { DocumentProcessorServiceClient } =
+  require("@google-cloud/documentai").v1;
 
 // Instantiates a client
 // apiEndpoint regions available: eu-documentai.googleapis.com, us-documentai.googleapis.com (Required if using eu based processor)
-const client = new DocumentProcessorServiceClient({apiEndpoint: 'https://us-documentai.googleapis.com/v1/projects/651028962226/locations/us/processors/164bb6b240e87770:process?key=71eff1f9a4b4e94de834be2aacdc00798fd168b8'});
+const client = new DocumentProcessorServiceClient({
+  apiEndpoint: "us-documentai.googleapis.com",
+});
+
 // const client = new DocumentProcessorServiceClient();
 
 quickstart();
@@ -23,32 +26,33 @@ async function quickstart() {
   const name = `projects/${projectId}/locations/${location}/processors/${processorId}`;
 
   // Read the file into memory.
-  const fs = require('fs').promises;
+  const fs = require("fs").promises;
   const imageFile = await fs.readFile(filePath);
 
   // Convert the image data to a Buffer and base64 encode it.
-  const encodedImage = Buffer.from(imageFile).toString('base64');
+  const encodedImage = Buffer.from(imageFile).toString("base64");
 
   const request = {
     name,
     rawDocument: {
       content: encodedImage,
-      mimeType: 'application/pdf',
+      mimeType: "application/pdf",
     },
+    key: "71eff1f9a4b4e94de834be2aacdc00798fd168b8",
   };
 
   // Recognizes text entities in the PDF document
   const [result] = await client.processDocument(request);
-  const {document} = result;
+  const { document } = result;
 
   // Get all of the document text as one big string
-  const {text} = document;
+  const { text } = document;
   console.log("TESTING");
 
   // Extract shards from the text field
-  const getText = textAnchor => {
+  const getText = (textAnchor) => {
     if (!textAnchor.textSegments || textAnchor.textSegments.length === 0) {
-      return '';
+      return "";
     }
 
     // First shard in document doesn't have startIndex property
@@ -59,9 +63,9 @@ async function quickstart() {
   };
 
   // Read the text recognition output from the processor
-  console.log('The document contains the following paragraphs:');
+  console.log("The document contains the following paragraphs:");
   const [page1] = document.pages;
-  const {paragraphs} = page1;
+  const { paragraphs } = page1;
 
   for (const paragraph of paragraphs) {
     const paragraphText = getText(paragraph.layout.textAnchor);
